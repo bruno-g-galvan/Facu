ea1041f325f475039e4d83f4183f5a84998c73b3
Bruno <bruno.gab.galvan@gmail.com>
2024-08-08 11:34:06 -0300

    ejer 4

--- 2C/Programacion 1/04.py
+++ 2C/Programacion 1/04.py
@@ -9,3 +9,39 @@
 # con $5000, el vuelto debe contener 1 billete de $1000, 1 billete de $500, 1 billete 
 # de $200, 1 billete de $100 y 3 billetes de $10.
 
+def calcularVuelto(total, recibido):
+    vuelto = recibido - total
+    lstBilletes = [5000, 1000, 500, 200, 100, 50, 10]
+    lstVuelto = [0, 0, 0, 0, 0, 0, 0]
+
+    for i in range(len(lstBilletes)):
+        while vuelto >= lstBilletes[i]:
+            vuelto = vuelto - lstBilletes[i]
+            lstVuelto[i] = lstVuelto[i] + 1
+
+    return lstVuelto
+    
+total = int(input("Ingrese el total de la compra: "))
+
+while total <= 0:
+    total = int(input("Ingrese un total de la compra valido mayor a 0: "))
+
+recibido = int(input("Ingrese el dinero recibido: "))
+
+while recibido < total:
+    recibido = int(input("Se debe recibir el dinero justo o mas que el total de la compra : "))
+
+listaVuelto = calcularVuelto(total, recibido)
+
+print("--- Vuelto ---")
+print("Cant. de billetes de 5000:", listaVuelto[0])
+print("Cant. de billetes de 1000:", listaVuelto[1])
+print("Cant. de billetes de 500:", listaVuelto[2])
+print("Cant. de billetes de 200:", listaVuelto[3])
+print("Cant. de billetes de 100:", listaVuelto[4])
+print("Cant. de billetes de 50:", listaVuelto[5])
+print("Cant. de billetes de 10:", listaVuelto[6])
+
+
+
+
